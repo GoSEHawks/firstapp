1894c32aa234e6b6c3c2100c93065fe5cc7db856
Ilya Nazarchuk <45705923+GoSEHawks@users.noreply.github.com>
2023-06-10 08:23:17 -0500

    Split component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
-import './ExpenseItem.css';
-function ExpenseItem() {
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>Date</div>
+        <ExpenseDate/>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
